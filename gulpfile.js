67c056f104a5518bd956ebbc574464d2471a395f
Briws <briws@pm.me>
2020-06-08 15:15:14 +0300

    Gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var
 	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass-compile', function(){
-	return gulp.src('./sass/**/*.sass')
+	return gulp.src('source/sass/*.sass')
 	.pipe(sourcemaps.init())
 	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer({
@@ -15,33 +15,33 @@ gulp.task('sass-compile', function(){
 		cascade: false
 	}))
 	.pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest('./release/css/'))
+	.pipe(gulp.dest('docs/css/'))
 })
 
 gulp.task('pug', function(){
-	return gulp.src('./*.pug')
+	return gulp.src('source/*.pug')
 		.pipe(pug({
 			pretty: true
 		}))
-		.pipe(gulp.dest('./release/'))
+		.pipe(gulp.dest('docs/'))
 })
 
 gulp.task('reload-css', function() {
-	return gulp.src('./release/css/*.css')
+	return gulp.src('docs/css/*.css')
 	.pipe(livereload())
 });
 
 gulp.task('reload-html', function() {
-	return gulp.src('./release/*.html')
+	return gulp.src('docs/*.html')
 	.pipe(livereload())
 	//
 });
 
 
 gulp.task('default', function(){
-	gulp.watch('./sass/**/*.sass', gulp.series('sass-compile'))
-	gulp.watch('./*.pug', gulp.series('pug'))
+	gulp.watch('source/sass/*.sass', gulp.series('sass-compile'))
+	gulp.watch('source/**/*.pug', gulp.series('pug'))
 	livereload.listen()
-	gulp.watch('./release/css/*.css', gulp.series('reload-css'))
-	gulp.watch('./release/*.html', gulp.series('reload-html'))
+	gulp.watch('docs/css/*.css', gulp.series('reload-css'))
+	gulp.watch('docs/*.html', gulp.series('reload-html'))
 })
